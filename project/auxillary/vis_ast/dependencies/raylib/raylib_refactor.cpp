b22f5892032545fbdb1a344ed1934202765de27a
Ed_ <edwardgz@gmail.com>
2023-10-24 12:46:59 -0400

    Progress on preparing raylib for usage

---
+++ project/auxillary/vis_ast/dependencies/raylib/raylib_refactor.cpp
@@ -0,0 +1,56 @@
+#define GEN_DEFINE_LIBRARY_CODE_CONSTANTS
+#define GEN_BENCHMARK
+#define GEN_ENFORCE_STRONG_CODE_TYPES
+#define GEN_IMPLEMENTATION
+#include "gen.hpp"
+
+
+constexpr char const* path_config    = "config.h";
+constexpr char const* path_raylib    = "raylib.h";
+constexpr char const* path_raymath   = "raymath.h";
+constexpr char const* path_rcore     = "rcore.h";
+constexpr char const* path_rgestures = "rgestures.h";
+constexpr char const* path_rgl       = "rgl.h";
+constexpr char const* path_rtext     = "rtext.h";
+
+
+using namespace gen;
+
+
+void refactor_file( CodeBody code_file )
+{
+	String name_scratch = String::make_reserve( GlobalAllocator, kilobytes(1) );
+	for ( Code elem : code_file )
+	{
+		if ( elem->Type == ECode::Preprocess_Define )
+		{
+			if ( str_compare( elem->Name, txt("RL_"), 2 ) == 0 )
+				continue;
+
+			name_scratch.append_fmt( "%RL_%S", elem->Name );
+			elem->Name = get_cached_string( name_scratch );
+			name_scratch.clear();
+		}
+	}
+}
+
+int gen_main()
+{
+	gen::init();
+
+	FileContents config_file_content = file_read_contents( GlobalAllocator, true, path_config );
+	CodeBody     config_code         = parse_global_body( { config_file_content.size, rcast(char const*, config_file_content.data) } );
+	refactor_file( config_code );
+	Builder config_builder = Builder::open( path_config );
+	config_builder.print( config_code );
+	config_builder.write();
+
+	FileContents raylib_file_content = file_read_contents( GlobalAllocator, true, path_raylib );
+	CodeBody     raylib_code         = parse_global_body( { raylib_file_content.size, rcast(char const*, raylib_file_content.data) } );
+	refactor_file( raylib_code );
+	Builder raylib_builder = Builder::open( path_raylib );
+	config_builder.print( raylib_code );
+	config_builder.write();
+
+	return 0;
+}
--- project/auxillary/vis_ast/update_deps.ps1
+++ project/auxillary/vis_ast/update_deps.ps1
@@ -49,8 +49,8 @@ switch ($_){
 . $incremental_checks
 
 # Clear out the current content first
-remove-item $path_temp -Recurse
-New-Item -ItemType Directory -Path $path_temp
+# remove-item $path_temp -Recurse
+# New-Item -ItemType Directory -Path $path_temp
 
 if ( -not (Test-Path $path_binaries) ) {
 	New-Item -ItemType Directory -Path $path_binaries
@@ -70,12 +70,13 @@ function setup-raylib {
 	$url_raylib_zip  = 'https://github.com/raysan5/raylib/archive/refs/heads/master.zip'
 	$path_raylib_zip = join-path $path_temp 'raylib.zip'
 
-	invoke-webrequest -uri $url_raylib_zip   -outfile $path_raylib_zip
-	expand-archive    -path $path_raylib_zip -destinationpath $path_temp
-
 	$path_raylib_master      = join-path $path_temp          'raylib-master'
 	$path_raylib_src         = join-path $path_raylib_master 'src'
 	$path_raylib_glfw_inc    = join-path $path_raylib_src    'external/glfw/include'
+
+	remove-item $path_raylib_master -Recurse
+	# invoke-webrequest -uri $url_raylib_zip   -outfile $path_raylib_zip
+	expand-archive    -path $path_raylib_zip -destinationpath $path_temp
 
 	write-host "Building raylib with $vendor"
 
@@ -84,41 +85,77 @@ function setup-raylib {
 		New-Item $path_build -ItemType Directory
 	}
 
-	# Microsoft
-	$lib_gdi32   = 'Gdi32.lib'
-	$lib_shell32 = 'Shell32.lib'
-	$lib_xinput  = 'Xinput.lib'
-	$lib_user32  = 'User32.lib'
-	$lib_winmm   = 'Winmm.lib'
+	# Refactor raylib
+	if ( $true ) {
+		$path_gencpp = join-path $path_root 'singleheader/gen'
 
-	$includes = @(
-		$path_raylib_src,
-		$path_raylib_glfw_inc
-	)
-	foreach ($include in $includes) {
-		write-host $include
+		$includes = @(
+			$path_gencpp
+		)
+
+		$compiler_args = @(
+			($flag_define + 'GEN_TIME')
+		)
+
+		$linker_args = @(
+
+		)
+
+		$unit       = join-path $path_raylib 'raylib_refactor.cpp'
+		$executable = join-path $path_build  'raylib_refactor.exe'
+
+		$build_result = build-simple $path_build $includes $compiler_args $linker_args $unit $executable
+		Push-Location $path_raylib_src
+			if ( Test-Path( $executable ) ) {
+				$time_taken = Measure-Command { & $executable
+						| ForEach-Object {
+							write-host `t $_ -ForegroundColor Green
+						}
+					}
+			}
+		Pop-Location
 	}
 
-	$compiler_args = @(
-		($flag_define + 'PLATFORM_DESKTOP')
-	)
-	$linker_args   = @(
-		$flag_link_dll,
+	# Build raylib
+	if ( $false ) {
+		# Microsoft
+		$lib_gdi32   = 'Gdi32.lib'
+		$lib_shell32 = 'Shell32.lib'
+		$lib_xinput  = 'Xinput.lib'
+		$lib_user32  = 'User32.lib'
+		$lib_winmm   = 'Winmm.lib'
 
-		# $lib_xinput,
-		$lib_gdi32,
-		$lib_shell32,
-		$lib_user32,
-		$lib_winmm
-	)
+		$includes = @(
+			$path_raylib_src,
+			$path_raylib_glfw_inc
+		)
+		foreach ($include in $includes) {
+			write-host $include
+		}
 
-	# $unit = join-path $path_raylib     'raylib.c'
-	$dll  = join-path $path_raylib_lib 'raylib.dll'
-	# $build_result = build-simple $path_build $includes $compiler_args $linker_args $unit $dll
+		$compiler_args = @(
+			($flag_define + 'PLATFORM_DESKTOP'),
+			($flag_define + 'BUILD_LIBTYPE_SHARED')
+		)
+		$linker_args   = @(
+			$flag_link_dll,
 
-	$raylib_modules = get-childitem -path $path_raylib_src -filter "*.c" -file
-	$build_result = build $path_build $includes $compiler_args $linker_args $raylib_modules $dll
+			# $lib_xinput,
+			$lib_gdi32,
+			$lib_shell32,
+			$lib_user32,
+			$lib_winmm
+		)
 
+		# $unit = join-path $path_raylib     'raylib.c'
+		$dll  = join-path $path_raylib_lib 'raylib.dll'
+		# $build_result = build-simple $path_build $includes $compiler_args $linker_args $unit $dll
+
+		$raylib_modules = get-childitem -path $path_raylib_src -filter "*.c" -file
+		$build_result = build $path_build $includes $compiler_args $linker_args $raylib_modules $dll
+	}
+
+	# Move headers to used include
 	$raylib_headers = Get-ChildItem -Path $path_raylib_src -Filter "*.h" -File
 	foreach ($header in $raylib_headers) {
 		Copy-Item -Path $header -Destination (join-path $path_raylib_inc (split-path $header -Leaf))
